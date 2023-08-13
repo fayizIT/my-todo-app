0b547d3b7318631a93496d3f8b1de4efc1816ef7
Fayiz <fayizcj94.com>
2023-08-13 19:10:28 +0530

    Tudo using React js

---
+++ src/components/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenSquare } from '@fortawesome/free-solid-svg-icons'; // Corrected icon import
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+export const Todo = ({ task, deleteTodo, editTodo, toggleComplete }) => {
+ 
+  return (
+    <div className="Todo">
+        <p className={`${task.completed ? 'completed' : ""}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+        <div>
+        <FontAwesomeIcon icon={faPenSquare} onClick={() => editTodo(task.id)} />
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
+        </div>
+    </div>
+  );
+};
